ff2c1ac2469da6f4925e76d4de469a1a0dfdee14
x_oli <x_oliver_x@hotmail.de>
2018-10-17 16:37:52 +0200

    init intellij project with kotlin and libgdx for desktop

---
+++ desktop/src/at/fhhgb/breakout/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package at.fhhgb.breakout.desktop
+
+import at.fhhgb.breakout.Game
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(Game(), config)
+    }
+}
